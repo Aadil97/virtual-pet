180f194d9c3efa6a5238aab7c8b8f195a2c4598b
Aadil <Aadil97@hotmail.co.uk>
2019-09-16 19:29:03 +0100

    Added a more data types to pet constructor and added walk function

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -7,3 +7,26 @@ describe('growUp', () => {
       expect(pet.age).toEqual(1);
     });
   });
+  describe('constructor', () => {
+    //...
+  
+    it('has a initial age of 0', () => {
+      const pet = new Pet('Fido');
+      expect(pet.age).toEqual(0);
+    });
+  });
+  describe('growUp', () => {
+    it('increments the age by 1', () => {
+      const pet = new Pet('Fido');
+      pet.growUp();
+      expect(pet.age).toEqual(1);
+    });
+  });
+  describe('walk', () => {
+    it('increases fitness by 4', () => {
+      const pet = new Pet('fido');
+      pet.fitness = 4;
+      pet.walk();
+      expect(pet.fitness).toEqual(8);
+    });
+  });
